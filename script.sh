2516ba6929f650625fd5c18ea6d0b48cde81492e
Promyse Ferguson <promyseferguson@MacBook-Pro.local>
2024-12-04 09:22:40 -0500

    making changes to files

--- script.sh
+++ script.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 # shell script for installation of required services & tools. 
-sudo apt-get update
-sudo apt-get install docker.io -y
-git clone https://github.com/N4si/DevSecOps-Project.git
-sudo usermod -aG docker $USER
-sudo apt-get install wget apt-transport-https gnupg lsb-release
-wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
-echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
-sudo apt-get install trivy
-sudo apt install openjdk-17-jre
-sudo apt-get install jenkins
-sudo systemctl enable jenkins
-sudo systemctl start jenkins
+apt-get update
+apt-get install docker.io -y
+git clone https://github.com/pjferguson/DevSecOps-Netflix-Clone.git
+usermod -aG docker $USER
+apt-get install wget apt-transport-https gnupg lsb-release
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key |  apt-key add -
+echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main |  tee -a /etc/apt/sources.list.d/trivy.list
+apt-get install trivy
+apt install openjdk-17-jre
+apt-get install jenkins
+systemctl enable jenkins
+systemctl start jenkins
 # Installation of jcli which will allow me to install required plugins.
 wget http://localhost:8080/jnlpJars/jenkins-cli.jar
 java -jar jenkins-cli.jar -s http://localhost:8080/ install-plugin adoptopenjdk:1.5
@@ -24,12 +24,13 @@ java -jar jenkins-cli.jar -s http://localhost:8080/ install-plugin docker-common
 java -jar jenkins-cli.jar -s http://localhost:8080/ install-plugin docker-java-api:3.4.0-94.v65ced49b_a_7d5
 
 # creating use "promonitoring for prometheus monitoring"
-sudo useradd --system promonitoring
-sudo apt-get -y install grafana
-sudo systemctl enable grafana-server
-sudo systemctl enable grafana-server
-sudo su 
-sudo usermod -aG docker jenkins
-sudo systemctl restart jenkins
+useradd --system promonitoring
+apt-get -y install grafana
+systemctl enable grafana-server
+systemctl enable grafana-server
+usermod -aG docker jenkins
+systemctl restart jenkins
+cd /DevSecOps-Netflix-Clone && chmod 755 docker.sh
+./docker.sh
 
 
